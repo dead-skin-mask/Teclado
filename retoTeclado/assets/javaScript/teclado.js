738106a4975275d07ac518171997a7aecf044640
Martinez_Julian <59780954+dead-skin-mask@users.noreply.github.com>
2021-03-17 20:50:02 -0500

    segundo commit

--- retoTeclado/assets/javaScript/teclado.js
+++ retoTeclado/assets/javaScript/teclado.js
@@ -8,6 +8,29 @@ let btn7 = document.getElementById ("btn7");
 let btn8 = document.getElementById ("btn8");
 let btn9 = document.getElementById ("btn9");
 let btn0 = document.getElementById ("btn0");
+//LETRAS
+let btnQ = document.getElementById ("btnQ");
+let btnW = document.getElementById ("btnW");
+let btnE = document.getElementById ("btnE");
+let btnR = document.getElementById ("btnR");
+let btnT = document.getElementById ("btnT");
+let btnY = document.getElementById ("btnY");
+let btnU = document.getElementById ("btnU");
+let btnI = document.getElementById ("btnI");
+let btnO = document.getElementById ("btnO");
+let btnP = document.getElementById ("btnP");
+//LETRAS 2 
+let btnA = document.getElementById ("btnA");
+let btnS = document.getElementById ("btnS");
+let btnD = document.getElementById ("btnD");
+let btnF = document.getElementById ("btnF");
+let btnG = document.getElementById ("btnG");
+let btnH = document.getElementById ("btnH");
+let btnJ = document.getElementById ("btnJ");
+let btnK = document.getElementById ("btnK");
+let btnL = document.getElementById ("btnL");
+let btnÑ = document.getElementById ("btnÑ");
+
 let tablero = document.getElementById ("tablero");
 let teclaTemp = ""
 
@@ -81,6 +104,129 @@ const btn0f = () => {
     console.log(teclaTemp);
 }
 
+//FUNCIONES LETRAS
+const btnQf = () => {
+    tablero.innerHTML = "Q"
+    tablero.innerHTML = teclaTemp + "Q";
+    teclaTemp = teclaTemp + "Q";
+    console.log(teclaTemp);
+}
+const btnWf = () => {
+    tablero.innerHTML = "W"
+    tablero.innerHTML = teclaTemp + "W";
+    teclaTemp = teclaTemp + "W";
+    console.log(teclaTemp);
+}
+const btnEf = () => {
+    tablero.innerHTML = "E"
+    tablero.innerHTML = teclaTemp + "E";
+    teclaTemp = teclaTemp + "E";
+    console.log(teclaTemp);
+}
+const btnRf = () => {
+    tablero.innerHTML = "R"
+    tablero.innerHTML = teclaTemp + "R";
+    teclaTemp = teclaTemp + "R";
+    console.log(teclaTemp);
+}
+const btnTf = () => {
+    tablero.innerHTML = "T"
+    tablero.innerHTML = teclaTemp + "T";
+    teclaTemp = teclaTemp + "T";
+    console.log(teclaTemp);
+}
+const btnYf = () => {
+    tablero.innerHTML = "Y"
+    tablero.innerHTML = teclaTemp + "Y";
+    teclaTemp = teclaTemp + "Y";
+    console.log(teclaTemp);
+}
+const btnUf = () => {
+    tablero.innerHTML = "U"
+    tablero.innerHTML = teclaTemp + "U";
+    teclaTemp = teclaTemp + "U";
+    console.log(teclaTemp);
+}
+const btnIf = () => {
+    tablero.innerHTML = "I"
+    tablero.innerHTML = teclaTemp + "I";
+    teclaTemp = teclaTemp + "I";
+    console.log(teclaTemp);
+}
+const btnOf = () => {
+    tablero.innerHTML = "O"
+    tablero.innerHTML = teclaTemp + "O";
+    teclaTemp = teclaTemp + "O";
+    console.log(teclaTemp);
+}
+const btnPf = () => {
+    tablero.innerHTML = "P"
+    tablero.innerHTML = teclaTemp + "P";
+    teclaTemp = teclaTemp + "P";
+    console.log(teclaTemp);
+}
+//FUNCIONES LETRAS 2
+const btnAf = () => {
+    tablero.innerHTML = "A"
+    tablero.innerHTML = teclaTemp + "A";
+    teclaTemp = teclaTemp + "A";
+    console.log(teclaTemp);
+}
+const btnSf = () => {
+    tablero.innerHTML = "S"
+    tablero.innerHTML = teclaTemp + "S";
+    teclaTemp = teclaTemp + "S";
+    console.log(teclaTemp);
+}
+const btnDf = () => {
+    tablero.innerHTML = "D"
+    tablero.innerHTML = teclaTemp + "D";
+    teclaTemp = teclaTemp + "D";
+    console.log(teclaTemp);
+}
+const btnFf = () => {
+    tablero.innerHTML = "F"
+    tablero.innerHTML = teclaTemp + "F";
+    teclaTemp = teclaTemp + "F";
+    console.log(teclaTemp);
+}
+const btnGf = () => {
+    tablero.innerHTML = "G"
+    tablero.innerHTML = teclaTemp + "G";
+    teclaTemp = teclaTemp + "G";
+    console.log(teclaTemp);
+}
+const btnHf = () => {
+    tablero.innerHTML = "H"
+    tablero.innerHTML = teclaTemp + "H";
+    teclaTemp = teclaTemp + "H";
+    console.log(teclaTemp);
+}
+const btnJf = () => {
+    tablero.innerHTML = "J"
+    tablero.innerHTML = teclaTemp + "J";
+    teclaTemp = teclaTemp + "J";
+    console.log(teclaTemp);
+}
+const btnKf = () => {
+    tablero.innerHTML = "K"
+    tablero.innerHTML = teclaTemp + "K";
+    teclaTemp = teclaTemp + "K";
+    console.log(teclaTemp);
+}
+const btnLf = () => {
+    tablero.innerHTML = "L"
+    tablero.innerHTML = teclaTemp + "L";
+    teclaTemp = teclaTemp + "L";
+    console.log(teclaTemp);
+}
+const btnÑf = () => {
+    tablero.innerHTML = "Ñ"
+    tablero.innerHTML = teclaTemp + "Ñ";
+    teclaTemp = teclaTemp + "Ñ";
+    console.log(teclaTemp);
+}
+
 btn1.onclick = function () {
     btn1f();
 }
@@ -119,4 +265,68 @@ btn9.onclick = function () {
 
 btn0.onclick = function () {
     btn0f();
+}
+
+//ONCLICKS LETRAS
+
+btnQ.onclick = function () {
+    btnQf();
+}
+btnW.onclick = function () {
+    btnWf();
+}
+btnE.onclick = function () {
+    btnEf();
+}
+btnR.onclick = function () {
+    btnRf();
+}
+btnT.onclick = function () {
+    btnTf();
+}
+btnY.onclick = function () {
+    btnYf();
+}
+btnU.onclick = function () {
+    btnUf();
+}
+btnI.onclick = function () {
+    btnIf();
+}
+btnO.onclick = function () {
+    btnOf();
+}
+btnP.onclick = function () {
+    btnPf();
+}
+//ONCLICKS LETRAS 2
+btnA.onclick = function () {
+    btnAf();
+}
+btnS.onclick = function () {
+    btnSf();
+}
+btnD.onclick = function () {
+    btnDf();
+}
+btnF.onclick = function () {
+    btnFf();
+}
+btnG.onclick = function () {
+    btnGf();
+}
+btnH.onclick = function () {
+    btnHf();
+}
+btnJ.onclick = function () {
+    btnJf();
+}
+btnK.onclick = function () {
+    btnKf();
+}
+btnL.onclick = function () {
+    btnLf();
+}
+btnÑ.onclick = function () {
+    btnÑf();
 }
